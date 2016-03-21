ffca942b9fbaac9343162824849351c6d9c0704a
German Lopez <german@toptierlabs.com>
2016-03-21 16:00:04 -0300

    Fixed communication manager response handling

--- swift-base/Networking/CommunicationManager.swift
+++ swift-base/Networking/CommunicationManager.swift
@@ -11,30 +11,34 @@ import Alamofire
 import SwiftyJSON
 
 class CommunicationManager {
-  
+
   private class func getHeader() -> [String: String]? {
     return UserDataManager.getSessionToken() != nil ? ["X-USER-TOKEN": "\(UserDataManager.getSessionToken()!)"] : nil
   }
-  
+
   private class func getBaseUrl() -> String {
     return NSBundle.mainBundle().objectForInfoDictionaryKey("Base URL") as? String ?? ""
   }
-  
+
+  private class func isOkStatus(code: Int?) -> Bool {
+    return Range(start: 200, end: 210).contains(code ?? 500)
+  }
+
   class func sendPostRequest(url url: String, params: [String: AnyObject]?, success:(responseObject: [String : AnyObject]) -> Void, failure:(error: NSError) -> Void) {
     let header = CommunicationManager.getHeader()
     let requestUrl = getBaseUrl() + url
     Alamofire.request(.POST, requestUrl, parameters: params, headers: header)
       .responseJSON { response in
         guard let JSONDictionary = response.result.value as? [String : AnyObject] else {
-          if response.result.isSuccess || Range(start: 200, end: 210).contains(response.response?.statusCode ?? 500) {
+          if response.result.isSuccess || isOkStatus(response.response?.statusCode) {
             success(responseObject: ["":""])
             return
           }
-          
+
           failure(error: NSError(domain: "Check your connection", code: 0, userInfo: nil))
           return
         }
-        if Range(start: 200, end: 210).contains(response.response?.statusCode ?? 500) {
+        if isOkStatus(response.response?.statusCode) {
           success(responseObject: JSONDictionary)
         } else {
           if let messageDict = JSONDictionary["error"] as? [String: [String]] {
@@ -47,23 +51,24 @@ class CommunicationManager {
         }
     }
   }
-  
+
   class func sendGetRequest(url url: String, params: [String: AnyObject]?, success: (responseObject: [String : AnyObject]) -> Void, failure: (error: NSError) -> Void) {
     let header = CommunicationManager.getHeader()
     let requestUrl = getBaseUrl() + url
     Alamofire.request(.GET, requestUrl, parameters: params, headers: header)
       .responseJSON { response in
-        
+
         guard let JSONDictionary = response.result.value as? [String : AnyObject] else {
-          if response.result.isSuccess || Range(start: 200, end: 210).contains(response.response?.statusCode ?? 500) {
+          if response.result.isSuccess || isOkStatus(response.response?.statusCode) {
             success(responseObject: ["":""])
             return
           }
-          
+
           failure(error: NSError(domain: "Check your connection", code: 0, userInfo: nil))
           return
         }
-        if Range(start: 200, end: 210).contains(response.response?.statusCode ?? 500) {
+
+        if isOkStatus(response.response?.statusCode) {
           success(responseObject: JSONDictionary)
         } else {
           if let messageDict = JSONDictionary["error"] as? [String: [String]] {
@@ -76,22 +81,24 @@ class CommunicationManager {
         }
     }
   }
-  
+
   class func sendPutRequest(url url: String, params: [String: AnyObject]?, success: (responseObject: [String : AnyObject]) -> Void, failure: (error: NSError) -> Void) {
     let header = CommunicationManager.getHeader()
     let requestUrl = getBaseUrl() + url
     Alamofire.request(.PUT, requestUrl, parameters: params, headers: header)
       .responseJSON { response in
         guard let JSONDictionary = response.result.value as? [String : AnyObject] else {
-          if response.result.isSuccess || Range(start: 200, end: 210).contains(response.response?.statusCode ?? 500) {
+
+          if response.result.isSuccess || isOkStatus(response.response?.statusCode) {
             success(responseObject: ["":""])
             return
           }
-          
+
           failure(error: NSError(domain: "Check your connection", code: 0, userInfo: nil))
           return
         }
-        if Range(start: 200, end: 210).contains(response.response?.statusCode ?? 500) {
+
+        if isOkStatus(response.response?.statusCode) {
           success(responseObject: JSONDictionary)
         } else {
           if let messageDict = JSONDictionary["error"] as? [String: [String]] {
