05113f74888091a764b228d4b99029f667af4fd9
German Lopez <german@toptierlabs.com>
2016-03-29 19:02:40 -0300

    Networking class changes

---
+++ swift-base/Helpers/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  swift-base
+//
+//  Created by German Lopez on 3/29/16.
+//  Copyright © 2016 TopTier labs. All rights reserved.
+//
+
+import Foundation
+
+//MARK: Media Types
+
+let videoType = "video"
+let imageType = "image"
--- swift-base/Helpers/PaginatedCollectionView.swift
+++ swift-base/Helpers/PaginatedCollectionView.swift
@@ -1,5 +1,5 @@
 //
-//  PaginatedTableView.swift
+//  PaginatedCollectionView.swift
 //  promptchal
 //
 //  Created by German Lopez on 3/22/16.
@@ -9,34 +9,26 @@
 import Foundation
 import UIKit
 
-protocol PaginatedTableViewDelegate {
+protocol PaginatedCollectionViewDelegate {
   
   // Required - Should not call this method directly or you will need to take care of
   // page update and flags status. Call loadContentIfNeeded instead
   func loadDataForPage(page: Int, completion: (elementsAdded: Int) -> Void)
 }
 
-enum PagingDirectionType: Int {
-  case AtBottom
-  case AtTop
-}
-
-class PaginatedTableView: UITableView {
+class PaginatedCollectionView: UICollectionView {
   
   var currentPage: Int = 1
-  var isLoading = false
+  var isLoading: Bool = false
   
   //  This will be handled automatically taking into account newElements of updateDelegate completion
   //  call and elementsPerPage. If your uploadDelegate provides pagination data, you can take control
   //  over this flag to avoid unnecesary calls to your delegate.
-  var hasMore = true
+  var hasMore: Bool = true
   var elementsPerPage: Int = 10
   
   // Responsible for loading the content and call the completion with newElements count.
   var updateDelegate: PaginatedTableViewDelegate!
-  
-  // Tells when pagination calls occurs. Options are when the tableView reaches bottom(.AtBottom) or top(.AtTop)
-  var direction: PagingDirectionType = .AtBottom
   
   override func awakeFromNib() {
     super.awakeFromNib()
@@ -46,8 +38,8 @@ class PaginatedTableView: UITableView {
     super.init(coder: aDecoder)
   }
   
-  override init(frame: CGRect, style: UITableViewStyle) {
-    super.init(frame: frame, style: style)
+  override init(frame: CGRect, collectionViewLayout layout: UICollectionViewLayout) {
+    super.init(frame: frame, collectionViewLayout: layout)
   }
   
   func loadContentIfNeeded() {
@@ -74,22 +66,37 @@ class PaginatedTableView: UITableView {
   func didScrollBeyondBottom() -> Bool {
     return contentOffset.y >= (contentSize.height - bounds.size.height)
   }
-}
-
-extension PaginatedTableView : UIScrollViewDelegate {
-
-  func scrollViewDidScroll(scrollView: UIScrollView) {
-    if direction == .AtBottom {
-      if didScrollBeyondTop() {
-        return
-      }else if didScrollBeyondBottom() {
-        loadContentIfNeeded()
-      }
-    }else {
-      if didScrollBeyondTop() {
-        loadContentIfNeeded()
-      }
-    }
+  
+  func didScrollBeyondLeft() -> Bool {
+    return contentOffset.x < 0
   }
   
+  func didScrollBeyondRight() -> Bool {
+    return contentOffset.x >= (contentSize.width - bounds.size.width)
+  }
+  
+}
+
+extension PaginatedCollectionView : UIScrollViewDelegate {
+  
+  func scrollViewDidScroll(scrollView: UIScrollView) {
+    
+    if let layout = collectionViewLayout as? UICollectionViewFlowLayout {
+      let direction = layout.scrollDirection
+      if direction == .Vertical {
+        if didScrollBeyondTop() {
+          return
+        }else if didScrollBeyondBottom() {
+          loadContentIfNeeded()
+        }
+      }else {
+        if didScrollBeyondLeft() {
+          return
+        }else if didScrollBeyondRight() {
+          loadContentIfNeeded()
+        }
+      }
+    }
+
+  }
 }
