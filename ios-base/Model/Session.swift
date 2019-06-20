fa891df8a2a083387fd5a083acc13cfda8dd7f56
Mauricio Cousillas <mauricio.cousillas@rootstrap.com>
2019-06-20 14:57:30 -0300

    Removed unnecessary code from codable clases

--- ios-base/Model/Session.swift
+++ ios-base/Model/Session.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-class Session: Codable {
+struct Session: Codable {
   var uid: String?
   var client: String?
   var accessToken: String?
@@ -41,28 +41,5 @@ class Session: Codable {
     uid = stringHeaders[HTTPHeader.uid.rawValue]
     client = stringHeaders[HTTPHeader.client.rawValue]
     accessToken = stringHeaders[HTTPHeader.token.rawValue]
-  }
-  
-  //MARK: Codable
-  
-  func encode(to encoder: Encoder) throws {
-    var container = encoder.container(keyedBy: CodingKeys.self)
-    try container.encode(uid, forKey: .uid)
-    try container.encode(client, forKey: .client)
-    try container.encode(accessToken, forKey: .accessToken)
-    try container.encode(expiry?.timeIntervalSince1970, forKey: .expiry)
-  }
-  
-  required init(from decoder: Decoder) throws {
-    let container = try decoder.container(keyedBy: CodingKeys.self)
-    uid = try container.decode(String.self, forKey: .uid)
-    client = try container.decode(String.self, forKey: .client)
-    accessToken = try container.decode(String.self, forKey: .accessToken)
-    do {
-      let value = try container.decode(Double.self, forKey: .expiry)
-      expiry = Date(timeIntervalSince1970: value)
-    } catch {
-      //Do nothing, expiry can be nil
-    }
   }
 }
